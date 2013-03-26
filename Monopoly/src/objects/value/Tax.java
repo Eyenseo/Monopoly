a206b175641185b59ca756440033546b67e9178e
Eyenseo <eyenseo@gmail.com>
2013-03-26 17:19:02 +0100

    Initialisation

---
+++ Monopoly/src/objects/value/Tax.java
@@ -0,0 +1,11 @@
+package objects.value;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eyenseo
+ * Date: 26.03.13
+ * Time: 17:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tax extends Restrictet {
+}
