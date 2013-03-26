f00da7bbdfbe12c63a0dc1c4a95def7da92e05b9
Eyenseo <eyenseo@gmail.com>
2013-03-26 17:15:28 +0100

    Initialisation

---
+++ Monopoly/src/objects/value/Jail.java
@@ -0,0 +1,11 @@
+package objects.value;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eyenseo
+ * Date: 26.03.13
+ * Time: 17:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class Jail extends Restrictet {
+}
---
+++ Monopoly/src/objects/value/Ownable.java
@@ -0,0 +1,11 @@
+package objects.value;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eyenseo
+ * Date: 26.03.13
+ * Time: 17:07
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Ownable extends Field {
+}
