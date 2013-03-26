6247e50f2da114abcddeb97c324455a5fd1f04b9
Eyenseo <eyenseo@gmail.com>
2013-03-26 17:17:23 +0100

    Initialisation

--- Monopoly/src/objects/value/Go.java
+++ Monopoly/src/objects/value/Go.java
@@ -7,5 +7,5 @@ package objects.value;
  * Time: 17:13
  * To change this template use File | Settings | File Templates.
  */
-public class Start extends Restrictet {
+public class Go extends Restrictet {
 }
