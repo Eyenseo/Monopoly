bcb88455b8af46740c78f5deade045d2bc5aea59
Eyenseo <eyenseo@gmail.com>
2013-03-26 16:53:37 +0100

    Initialisation

---
+++ Monopoly/src/core/Monopoly.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eyenseo
+ * Date: 26.03.13
+ * Time: 16:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class Monopoly {
+}
