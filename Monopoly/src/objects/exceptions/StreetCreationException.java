e0e9f96e682e9124207b4c93e5862cc4fd567a3b
Maikel <jpsh1991@gmx.net>
2013-04-10 12:45:48 +0200

    groß und kleinschreibung :P

--- Monopoly/src/objects/exceptions/StreetCreationException.java
+++ Monopoly/src/objects/exceptions/StreetCreationException.java
@@ -7,12 +7,12 @@ package objects.exceptions;
  * @version 1
  */
 public class StreetCreationException extends StorageReaderException {
-	/**
-	 * @param name  The value determines the name, if it exists, of the Street with the problem.
-	 * @param cause The value determines the previous Exception.
-	 */
-	public StreetCreationException(String name, Throwable cause) {
-		super((name != null) ? "Something was missing while reading the data of Street:\n\t" + name
-				      : "There should have been data for a Street but there wasn't!", cause);
-	}
+    /**
+     * @param name  The value determines the name, if it exists, of the Street with the problem.
+     * @param cause The value determines the previous Exception.
+     */
+    public StreetCreationException(String name, Throwable cause) {
+        super((name != null) ? "Something was missing while reading the data of street:\n\t" + name
+                : "There should have been data for a street but there wasn't!", cause);
+    }
 }
