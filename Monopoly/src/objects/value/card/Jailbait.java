2583fc2f7eb725492e9a811d1c31fb7a8626c9b7
Maikel <jpsh1991@gmx.net>
2013-04-16 10:49:18 +0200

    fleshed out "jailbait card"

--- Monopoly/src/objects/value/card/Jailbait.java
+++ Monopoly/src/objects/value/card/Jailbait.java
@@ -1,13 +1,16 @@
 package objects.value.card;
 
-/** Jailbait is the jailbait card (Gefängnisfreikarte). */
+/**
+ * Jailbait is the jailbait card (Gefängnisfreikarte).
+ * A card is a trigger for a specific event defined in the gameplay mechanics
+ */
 public class Jailbait extends Card {
-	/**
-	 * @param name Name of the card
-	 * @param text Text of the card
-	 */
-	public Jailbait(String name, String text) {
-		super(name, text);
-	}
-	// TODO everything
+    /**
+     * @param name Name of the card
+     * @param text Text of the card
+     */
+    public Jailbait(String name, String text) {
+        super(name, text);
+    }
+
 }
