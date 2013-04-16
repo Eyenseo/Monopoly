4c6158b7eb69f1b79c70ba86b981cd4fcfa7c9f0
Maikel <jpsh1991@gmx.net>
2013-04-16 12:05:21 +0200

    keywords changed, CardCreater created

---
+++ Monopoly/src/core/data/CardCreator.java
@@ -0,0 +1,14 @@
+package core.data;
+
+import objects.exceptions.EndOfBlockException;
+import objects.exceptions.StartOfBlockException;
+import objects.exceptions.StorageReaderException;
+import objects.exceptions.EndOfFileException;
+import objects.exceptions.map.StreetCreationException;
+import objects.value.map.StreetCircularList;
+
+public class CardCreator extends StorageReader {
+
+
+}
+
