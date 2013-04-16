fa4f71f1a4ab8566edd67b9d9ac9fbb700e526fe
Maikel <jpsh1991@gmx.net>
2013-04-16 11:12:38 +0200

    event cards added

---
+++ Monopoly/src/objects/value/card/GoToStation.java
@@ -0,0 +1,16 @@
+package objects.value.card;
+
+/**
+ * GoToStation is a special_movement card (Rücke vor bis zum nächsten Bahnhof).
+ * A card is a trigger for a specific event defined in the gameplay mechanics
+ */
+public class GoToStation extends Card {
+    /**
+     * @param name Name of the card
+     * @param text Text of the card
+     */
+    public GoToStation(String name, String text) {
+        super(name, text);
+    }
+
+}
---
+++ Monopoly/src/objects/value/card/SpecialPayment.java
@@ -0,0 +1,16 @@
+package objects.value.card;
+
+/**
+ * SpecialPayment is a special transaction card (player based transactions).
+ * A card is a trigger for a specific event defined in the gameplay mechanics
+ */
+public class SpecialPayment extends Card {
+    /**
+     * @param name Name of the card
+     * @param text Text of the card
+     */
+    public SpecialPayment(String name, String text) {
+        super(name, text);
+    }
+
+}
