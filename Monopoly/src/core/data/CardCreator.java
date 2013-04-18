Eyenseo/Monopoly

commit b80e42672868487d3d297ebb1f5638ce46fa085e

cardcreator added

Monopoly/src/core/data/CardCreator.java
@@ -1,6 +1,104 @@
 package core.data;
 
+import objects.card.Card;
+import objects.card.GoBack;
+import objects.card.GoTo;
+import objects.exceptions.EndOfBlockException;
+import objects.exceptions.StorageReaderException;
+import objects.exceptions.card.CardCreationException;
+
 public class CardCreator extends StorageReader {
+    public CardCreator(String file) {
+        super(file);
+    }
+
+    public CreateCardArray[]
+
+    {
+
+    }
+
+    private Card nextCard() throws CardCreationException {
+        String line = nextString();
+        if (isControlWord(line)) {
+            if (line.equals("#back")) {
+                return createGoBack();
+            }
+            if (line.equals("#goTo")) {
+                return createGoTo();
+            }
+            if (line.equals("#gotostation")) {
+                return createGoToStation();
+            }
+            if (line.equals("#jail")) {
+                return createJail();
+            }
+            if (line.equals("#jailbait")) {
+                return createJailbait();
+            }
+            if (line.equals("#payfinetakecard")) {
+                return createPayFineTakeCard();
+            }
+            if (line.equals("#$")) {
+                return createPayment();
+            }
+            if (line.equals("#special$")) {
+                return createSpecialPayment();
+            }
+            if (line.equals("#streetWork")) {
+                return createStreetWork();
+            }
+
+        }
+    }
+
+    private GoBack createGoBack() throws StorageReaderException {
+        String name = null;
+
+        try {
+            name = nextString();
+            String text = nextString();
+            int fields = nextInt();
+            if (!isEndOfBlock()) {
+                throw new EndOfBlockException(path);
+            }
+            return new GoBack(name, text, fields);
+        } catch (Exception e) {
+            throw new GoBackCreationException(name, e);
+        }
+    }
+
+    private GoTo createGoTo() throws StorageReaderException {
+        String name = null;
+        try {
+            name = nextString();
+            String text = nextString();
+            if (!isEndOfBlock()) {
+                throw new EndOfBlockException(path);
+            }
+            return new GoTo(name, text);
+        } catch (Exception e) {
+            throw new GoToCreationException(name, e);
+        }
+    }
+
+    private GoBack createGoToStation() throws StorageReaderException {
+        String name = null;
+
+        try {
+            name = nextString();
+            String text = nextString();
+            int fields = nextInt();
+            if (!isEndOfBlock()) {
+                throw new EndOfBlockException(path);
+            }
+            return new GoBack(name, text, fields);
+        } catch (Exception e) {
+            throw new GoBackCreationException(name, e);
+        }
+    }
+
 
 }
+
 

commit 85c33a4cfd36d275bd152be536be48dc21c47d86

Fix various errors connected with the CardCreator most errors introduced in commit b80e42672868487d3d297ebb1f5638ce46fa085e and b7d3a89d4edccacbe7291cf86103e76ba3ee7d9d

Monopoly/src/core/data/CardCreator.java
@@ -5,25 +5,35 @@ import objects.exceptions.EndOfBlockException;
 import objects.exceptions.StorageReaderException;
 import objects.exceptions.card.*;
 
-import java.util.List;
 import java.util.Vector;
 
+//TODO Doc
 public class CardCreator extends StorageReader {
-	public CardCreator(String file) {
+	private final String NAME;
+
+	//TODO Doc
+	public CardCreator(String file, String name) {
 		super(file);
+		NAME = name;
 	}
 
-	public Card[] cardArray() throws CardArrayException {
-		List<Card> cards = new Vector<Card>();
-		while(true) { //TODO Placeholder :)
-			cards.add(nextCard());
+	//TODO Doc
+	public Card[] cardArray() throws StorageReaderException {
+		Vector<Card> cardVector = new Vector<Card>();
+		Card[] cardArray;
+		Card temp;
+		while((temp = nextCard()) != null) {
+			cardVector.add(temp);
 		}
-		return (Card[]) cards.toArray();
+		cardArray = new Card[cardVector.size()];
+		cardVector.toArray(cardArray);
+		return cardArray;
 	}
 
+	//TODO Doc
 	private Card nextCard() throws StorageReaderException {
-		String line = nextString();
-		if(isControlWord(line)) {
+		String line = nextControllWord();
+		if(line != null) {
 			if(line.equals("#back")) {
 				return createGoBack();
 			}
@@ -51,143 +61,141 @@ public class CardCreator extends StorageReader {
 			if(line.equals("#streetWork")) {
 				return createStreetWork();
 			}
+			throw new CardCreationException(line);
 		}
+		return null;
 	}
 
+	//TODO Doc
 	private GoBack createGoBack() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
+			text = nextString();
 			int fields = nextInt();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new GoBack(name, text, fields);
+			return new GoBack(NAME, text, fields);
 		} catch(Exception e) {
-			throw new GoBackCreationException(name, e);
+			throw new GoBackCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private GoTo createGoTo() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
+			text = nextString();
+			String field = nextString();
+			boolean overGo = nextString().equals("t");
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new GoTo(name, text);
+			return new GoTo(NAME, text, field, overGo);
 		} catch(Exception e) {
-			throw new GoToCreationException(name, e);
+			throw new GoToCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private GoToStation createGoToStation() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
-			int fields = nextInt();
+			text = nextString();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new GoToStation(name, text);
+			return new GoToStation(NAME, text);
 		} catch(Exception e) {
-			throw new GoToStationCreationException(name, e);
+			throw new GoToStationCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private Jail createJail() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
-			int fields = nextInt();
+			text = nextString();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new Jail(name, text);
+			return new Jail(NAME, text);
 		} catch(Exception e) {
-			throw new JailCreationException(name, e);
+			throw new JailCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private Jailbait createJailbait() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
-			int fields = nextInt();
+			text = nextString();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new Jailbait(name, text);
+			return new Jailbait(NAME, text);
 		} catch(Exception e) {
-			throw new JailbaitCreationException(name, e);
+			throw new JailbaitCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private PayFineTakeCard createPayFineTakeCard() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
-			int fields = nextInt();
+			text = nextString();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new PayFineTakeCard(name, text);
+			return new PayFineTakeCard(NAME, text);
 		} catch(Exception e) {
-			throw new PayFineTakeCardCreationException(name, e);
+			throw new PayFineTakeCardCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private Payment createPayment() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
+			text = nextString();
 			int dm = nextInt();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new Payment(name, text, dm);
+			return new Payment(NAME, text, dm);
 		} catch(Exception e) {
-			throw new PaymentCreationException(name, e);
+			throw new PaymentCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private SpecialPayment createSpecialPayment() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
+			text = nextString();
 			int dm = nextInt();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new SpecialPayment(name, text, dm);
+			return new SpecialPayment(NAME, text, dm);
 		} catch(Exception e) {
-			throw new SpecialPaymentCreationException(name, e);
+			throw new SpecialPaymentCreationException(NAME, e);
 		}
 	}
 
+	//TODO Doc
 	private StreetWork createStreetWork() throws StorageReaderException {
-		String name = null;
+		String text = null;
 		try {
-			name = nextString();
-			String text = nextString();
+			text = nextString();
 			int dmHouse = nextInt();
 			int dmHotel = nextInt();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new StreetWork(name, text, dmHouse, dmHotel);
+			return new StreetWork(NAME, text, dmHouse, dmHotel);
 		} catch(Exception e) {
-			throw new StreetWorkCreationException(name, e);
+			throw new StreetWorkCreationException(NAME, e);
 		}
 	}
 }
-
-

Monopoly/src/objects/card/GoTo.java
@@ -5,11 +5,13 @@ package objects.card;
  * A card is a trigger for a specific event defined in the gameplay mechanics
  */
 public class GoTo extends Card {
-	/**
-	 * @param name Name of the card
-	 * @param text Text of the card
-	 */
-	public GoTo(String name, String text) {
+	private final String  FIELD;
+	private final boolean OVERGO;
+
+	//TODO Doc
+	public GoTo(String name, String text, String field, boolean overGo) {
 		super(name, text);
+		FIELD = field;
+		OVERGO = overGo;
 	}
 }

Monopoly/src/objects/exceptions/card/CardCreationException.java
@@ -2,9 +2,10 @@ package objects.exceptions.card;
 
 import objects.exceptions.StorageReaderException;
 
+//TODO Doc
 public class CardCreationException extends StorageReaderException {
-	public CardCreationException(String name, Throwable cause) {
-		super((name != null) ? "Something was missing while reading the data of a card:\n\t" + name
-		                     : "There should have been data for a card but there wasn't!", cause);
+	//TODO Doc
+	public CardCreationException(String word) {
+		super("There was an undefined control word:\n\t" + word);
 	}
 }