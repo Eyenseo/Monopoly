Eyenseo/Monopoly

commit 4df821a65e6154fd763b1631c222d50621dd5379

This breaks everything! Move from the Map class with array as base to Field as CircularList. Also the Connecting of the Street/Station/Facility Groups is done by a separate class which will also do the connecting stuff for the Cards. The basic list ist working, but breaks everything connected to the Map class.

Monopoly/src/core/Monopoly.java
@@ -4,8 +4,8 @@ import core.data.MapArrayCreator;
 import objects.Player;
 import objects.card.CardStack;
 import objects.exceptions.StorageReaderException;
-import objects.exceptions.map.MapInitialisationException;
-import objects.map.Map;
+import objects.map.Field;
+import objects.map.MapConnector;
 import objects.map.PurchasableCircularList;
 import objects.map.StreetCircularList;
 import ui.Menu;
@@ -17,30 +17,28 @@ import java.util.Vector;
 public class Monopoly {
 	private final int     STARTMONEY = 50000;
 	private       boolean gameOver   = false;
-	private Map  map;
-	private Menu menu;
+	private Field start;
+	private Menu  menu;
 	private Vector<Player> playerVector = new Vector<Player>();
 
 	//JAVADOC
 	Monopoly(Menu menu) {
 		try {
+			Field[] mapArray = new MapArrayCreator().createMapArray();
 			CardStack event = new CardStack("events.txt", "Event Karte");
 			CardStack community = new CardStack("community.txt", "Gemeinschafts Karte");
-			this.map = new Map(new MapArrayCreator().createMap());
+			this.start = new MapConnector().make(mapArray, event, community);
 			this.menu = menu;
 		} catch(StorageReaderException e) {
 			//TODO catch the exceptions properly
 			System.err.println(e.getMessageStack());
-		} catch(MapInitialisationException e) {
-			//TODO catch the exceptions properly
-			System.err.print(e.getMessage());
 		}
 	}
 
 	//JAVADOC
 	private void addPlayer(Player player) {
 		playerVector.add(player);
-		map.setPlayerToStart(player);
+		player.setField(start);
 	}
 
 	//JAVADOC
@@ -106,7 +104,7 @@ public class Monopoly {
 			}
 		}
 		if(doublesTime == 3) {
-			map.putInJail(player);
+			player.setField("Jail");
 			menu.inJail();
 		} else if(doubles) {
 			nextTurn(player, doublesTime + 1);

Monopoly/src/objects/Player.java
@@ -49,7 +49,7 @@ public class Player {
 			purchasable.getOwner().addMoney(purchasable.getPrice());
 		}
 		this.money -= purchasable.getPrice();
-		purchasable.setOwner(this);
+		//		purchasable.setOwner(this);
 		this.ownedFieldMap.put(purchasable.getName(), purchasable);
 	}
 
@@ -102,7 +102,15 @@ public class Player {
 
 	//JAVADOC
 	public void setField(Field field) {
+		setField(field, false);
+	}
+
+	//JAVADOC
+	public void setField(Field field, boolean doAction) {
 		this.field = field;
+		if(doAction) {
+			field.action(this);
+		}
 	}
 
 	//JAVADOC

Monopoly/src/objects/map/Field.java
@@ -1,37 +1,50 @@
 package objects.map;
-//JAVADOC
 
 import objects.Player;
 
-/**
- * @author Eyenseo
- * @version 0.1
- */
+//JAVADOC
 public abstract class Field {
-	private final String NAME;
+	private String name;
+	private Field  next;
+	private Field  previous;
 
-	/**
-	 * @param name The value determines the name of the object.
-	 */
+	//JAVADOC
 	Field(String name) {
-		this.NAME = name;
+		this.name = name;
+		this.next = this;
+		this.previous = this;
 	}
 
 	/**
 	 * @return The return value is the name.
 	 */
 	public String getName() {
-		return this.NAME;
+		return this.name;
 	}
 
 	//JAVADOC
 	@Override
 	public String toString() {
-		return this.NAME;
+		return this.name;
 	}
 
+	//JAVADOC
 	//TODO this has to be replaced in each individual Method and set to abstract
 	public void action(Player player) {
 		System.err.println("This is a placeholder lalala~lalaLA");
+	}
+
+	//JAVADOC
+	public void add(Field previous) {
+		if(previous.previous.equals(previous)) {
+			previous.next = this;
+			previous.previous = this;
+			this.next = previous;
+		} else {
+			this.next = previous.next;
+			this.next.previous = this;
+			previous.next = this;
+		}
+		this.previous = previous;
 	}
 }

Monopoly/src/objects/map/PurchasableCircularList.java
@@ -16,8 +16,8 @@ public abstract class PurchasableCircularList extends Field {
 	protected final int                     MORTGAGE;   //Hypothek
 	protected       Player                  owner;
 	protected       int                     stage;
-	protected       PurchasableCircularList next;
 	protected       boolean                 inMortgage;
+	protected       PurchasableCircularList nextGroupElement;
 
 	/**
 	 * @param name     The value determines the name of the object.
@@ -34,7 +34,7 @@ public abstract class PurchasableCircularList extends Field {
 		this.owner = null;
 		this.stage = 0;
 		this.inMortgage = false;
-		this.next = this;
+		this.nextGroupElement = this;
 	}
 
 	/**
@@ -43,16 +43,15 @@ public abstract class PurchasableCircularList extends Field {
 	public Player getOwner() {
 		return this.owner;
 	}
-
-	/**
-	 * @param owner The value determines the owner.
-	 */
-	public void setOwner(Player owner) {
-		this.owner = owner;
-		if(sameOwnerCheck(this)) {
-			stage = 1;
-		}
-	}
+	//	/**
+	//	 * @param owner The value determines the owner.
+	//	 */
+	//	public void setOwner(Player owner) {
+	//		this.owner = owner;
+	//		if(sameOwnerCheck(this)) {
+	//			stage = 1;
+	//		}
+	//	}
 
 	/**
 	 * @return The return value is the amount that has to be payed to become the owner.
@@ -92,37 +91,45 @@ public abstract class PurchasableCircularList extends Field {
 	public int getBill() {
 		return this.INCOME[this.stage];
 	}
-
-	/**
-	 * @param purchasable The value determines the next object in the circular list of objects that belong together.
-	 */
-	//TODO Move to constructor
-	public void add(PurchasableCircularList purchasable) {
-		purchasable.next = this.next;
-		this.next = purchasable;
-	}
-
-	/**
-	 * @return The return value is the reference to the next PurchasableCircularList object.
-	 */
-	private PurchasableCircularList getNext() {
-		return this.next;
-	}
-
-	//JAVADOC
-	private boolean sameOwnerCheck(PurchasableCircularList start) {
-		if(this.owner.equals(this.next.getOwner())) {
-			if(this.next.getNext().equals(start)) {
-				return true;
-			}
-			return this.next.sameOwnerCheck(start);
-		}
-		return false;
-	}
+	//	/**
+	//	 * @param purchasable The value determines the next object in the circular list of objects that belong together.
+	//	 */
+	//	//TODO Move to constructor
+	//	public void add(PurchasableCircularList purchasable) {
+	//		purchasable.next = this.next;
+	//		this.next = purchasable;
+	//	}
+	//
+	//	/**
+	//	 * @return The return value is the reference to the next PurchasableCircularList object.
+	//	 */
+	//	private PurchasableCircularList getNext() {
+	//		return this.next;
+	//	}
+	//
+	//	//JAVADOC
+	//	private boolean sameOwnerCheck(PurchasableCircularList start) {
+	//		if(this.owner.equals(this.next.getOwner())) {
+	//			if(this.next.getNext().equals(start)) {
+	//				return true;
+	//			}
+	//			return this.next.sameOwnerCheck(start);
+	//		}
+	//		return false;
+	//	}
 
 	//JAVADOC
 	@Override
 	public String toString() {
 		return this.getName() + ((getOwner() != null) ? " (" + getOwner() + ")" : "");
+	}
+
+	public PurchasableCircularList getNextGroupElement() {
+		return nextGroupElement;
+	}
+
+	public void setNextGroupElement(PurchasableCircularList nextGroupElement) {
+		nextGroupElement.nextGroupElement = this.nextGroupElement;
+		this.nextGroupElement = nextGroupElement;
 	}
 }

Monopoly/src/core/data/MapArrayCreator.java
@@ -33,7 +33,7 @@ public class MapArrayCreator extends StorageReader {
 	 * @see StorageReaderException
 	 */
 	//JAVADOC
-	public Field[] createMap() throws StorageReaderException {
+	public Field[] createMapArray() throws StorageReaderException {
 		Vector<Field> fieldVector = new Vector<Field>();
 		Field[] fieldArray;
 		Field temp;
@@ -117,13 +117,12 @@ public class MapArrayCreator extends StorageReader {
 				throw new EndOfBlockException(path);
 			}
 			StreetCircularList street = new StreetCircularList(name, price, income, mortgage, upgrade, color);
-			connectStreet(street);
+			//			connectStreet(street);
 			return street;
 		} catch(Exception e) {
 			throw new StreetCreationException(name, e);
 		}
 	}
-
 	/**
 	 * This method checks the last StreetCircularList object was already of it's color, if it is it connects the them,
 	 * if not the StreetCircularList object in the parameter is the 'start' of a new circular list.
@@ -131,13 +130,13 @@ public class MapArrayCreator extends StorageReader {
 	 * @param street The value determines the StreetCircularList object which will check if it belongs to the previous
 	 *               StreetCircularList object.
 	 */
-	private void connectStreet(StreetCircularList street) {
-		if(streetGroupBuffer != null && streetGroupBuffer.isSameColor(street)) {
-			streetGroupBuffer.add(street);
-		} else {  // The else block is for performance by letting go of the 'order' from the circular list
-			streetGroupBuffer = street;
-		}
-	}
+	//	private void connectStreet(StreetCircularList street) {
+	//		if(streetGroupBuffer != null && streetGroupBuffer.isSameColor(street)) {
+	//			streetGroupBuffer.add(street);
+	//		} else {  // The else block is for performance by letting go of the 'order' from the circular list
+	//			streetGroupBuffer = street;
+	//		}
+	//	}
 
 	/**
 	 * This method connects the new station with the other if other exist.
@@ -163,26 +162,25 @@ public class MapArrayCreator extends StorageReader {
 			}
 			//TODO get owner and stage from a save file
 			StationCircularList station = new StationCircularList(name, price, income, mortgage);
-			connectStation(station);
+			//			connectStation(station);
 			return station;
 		} catch(Exception e) {
 			throw new StationCreationException(name, e);
 		}
 	}
-
 	/**
 	 * This method connects the station with the next one if there was already one.
 	 *
 	 * @param station The value determines the StationCircularList object that will be added to the other
 	 *                StationCircularList objects.
 	 */
-	private void connectStation(StationCircularList station) {
-		if(stationGroupBuffer != null) {
-			stationGroupBuffer.add(station);
-		} else {// The else block is for performance by letting go of the 'order' from the circular list
-			stationGroupBuffer = station;
-		}
-	}
+	//	private void connectStation(StationCircularList station) {
+	//		if(stationGroupBuffer != null) {
+	//			stationGroupBuffer.add(station);
+	//		} else {// The else block is for performance by letting go of the 'order' from the circular list
+	//			stationGroupBuffer = station;
+	//		}
+	//	}
 
 	/**
 	 * This method connects the new facility with the other if other exist.
@@ -204,26 +202,25 @@ public class MapArrayCreator extends StorageReader {
 				throw new EndOfBlockException(path);
 			}
 			FacilityCircularList facility = new FacilityCircularList(name, price, income, mortgage);
-			connectFacility(facility);
+			//			connectFacility(facility);
 			return facility;
 		} catch(Exception e) {
 			throw new FacilityCreationException(name, e);
 		}
 	}
-
 	/**
 	 * This method connects the facility with the next one if there was already one.
 	 *
 	 * @param facility The value determines the FacilityCircularList object that will be added to the other
 	 *                 FacilityCircularList objects.
 	 */
-	private void connectFacility(FacilityCircularList facility) {
-		if(facilityGroupBuffer != null) {
-			facilityGroupBuffer.add(facility);
-		} else {// The else block is for performance by letting go of the 'order' from the circular list
-			facilityGroupBuffer = facility;
-		}
-	}
+	//	private void connectFacility(FacilityCircularList facility) {
+	//		if(facilityGroupBuffer != null) {
+	//			facilityGroupBuffer.add(facility);
+	//		} else {// The else block is for performance by letting go of the 'order' from the circular list
+	//			facilityGroupBuffer = facility;
+	//		}
+	//	}
 
 	/**
 	 * @return The return value is a Tax object based on the data in the storage package.

Monopoly/src/objects/map/MapConnector.java
@@ -1,0 +1,77 @@
+package objects.map;
+
+import objects.card.CardStack;
+
+import java.util.Vector;
+
+//JAVADOC
+public class MapConnector {
+
+	//JAVADOC
+	public Field make(Field[] map) {
+		connectAllFields(map);
+		connetGroups(map);
+		setDiceArray(map);
+		return map[0];
+	}
+
+	//JAVADOC
+	public Field make(Field[] map, CardStack event, CardStack community) {
+		connectAllFields(map);
+		connetGroups(map);
+		return map[0];
+	}
+
+	//JAVADOC
+	private void connectAllFields(Field[] map) {
+		Field prev = map[0];
+		for(int i = 1; i < map.length; i++) {
+			map[i].add(prev);
+			prev = map[i];
+		}
+	}
+
+	//JAVADOC
+	private void connetGroups(Field[] map) {
+		Vector<Field> fieldVector = new Vector<Field>();
+		for(Field f : map) {
+			fieldVector.addElement(f);
+		}
+		connetGroups(fieldVector);
+	}
+
+	//JAVADOC
+	private void connetGroups(Vector<Field> fieldVector) {
+		Field a = fieldVector.firstElement();
+		fieldVector.remove(fieldVector.indexOf(a));
+		if(a instanceof PurchasableCircularList) {
+			if(a instanceof StreetCircularList) {
+				for(Field b : fieldVector) {
+					if(b instanceof StreetCircularList && ((StreetCircularList) b).isSameColor(
+							(StreetCircularList) a)) {
+						((PurchasableCircularList) b).setNextGroupElement((PurchasableCircularList) a);
+					}
+				}
+			} else if(a instanceof StationCircularList) {
+				for(Field b : fieldVector) {
+					if(b instanceof StationCircularList) {
+						((PurchasableCircularList) b).setNextGroupElement((PurchasableCircularList) a);
+					}
+				}
+			} else if(a instanceof FacilityCircularList) {
+				for(Field b : fieldVector) {
+					if(b instanceof FacilityCircularList) {
+						((PurchasableCircularList) b).setNextGroupElement((PurchasableCircularList) a);
+					}
+				}
+			}
+		}
+		if(!fieldVector.isEmpty()) {
+			connetGroups(fieldVector);
+		}
+	}
+
+	//JAVADOC
+	private void setDiceArray(Field[] map) {
+	}
+}

Monopoly/testArea/MonoTest.java
@@ -1,0 +1,16 @@
+import core.data.MapArrayCreator;
+import objects.exceptions.StorageReaderException;
+import objects.map.Field;
+import objects.map.MapConnector;
+
+public class MonoTest {
+	public static void main(String[] args) {
+		Field f = null;
+		try {
+			f = new MapConnector().make(new MapArrayCreator().createMapArray());
+		} catch(StorageReaderException e) {
+			System.err.print(e.getMessageStack());
+		}
+		System.out.print("Set stop - check the map in debug.");
+	}
+}

commit 1da54f3c4b40075390ec5dfaf1736dc268496aa1

Fix problems introduced by commit 4df821a65e6154fd763b1631c222d50621dd5379 GoTo: Add attribute and setter for Field (Go)go. Connector: Fix a few bugs in the makeCircular_List and connectGoToJail methods. Fix setDiceArray method - adjust array length and initialise it by every for loop. Add connect GoTo with Go. Player: Add goToJail method, this should be only temporary.

Monopoly/src/core/Connector.java
@@ -91,8 +91,9 @@ public class Connector {
 		for(int i = 0; i < fieldArrayList.size(); i++) {
 			next = fieldArrayList.get(i);
 			if(next instanceof StreetCircularList && ((StreetCircularList) next).isSameColor(street)) {
-				((StreetCircularList) next).setNextGroupElement(street);
+				street.setNextGroupElement(((StreetCircularList) next));
 				fieldArrayList.remove(i);
+				i--;
 			}
 		}
 	}
@@ -103,8 +104,9 @@ public class Connector {
 		for(int i = 0; i < fieldArrayList.size(); i++) {
 			next = fieldArrayList.get(i);
 			if(next instanceof StationCircularList) {
-				((StationCircularList) next).setNextGroupElement(station);
+				station.setNextGroupElement(((StationCircularList) next));
 				fieldArrayList.remove(i);
+				i--;
 			}
 		}
 	}
@@ -115,8 +117,9 @@ public class Connector {
 		for(int i = 0; i < fieldArrayList.size(); i++) {
 			next = fieldArrayList.get(i);
 			if(next instanceof FacilityCircularList) {
-				((FacilityCircularList) next).setNextGroupElement(facility);
+				facility.setNextGroupElement(((FacilityCircularList) next));
 				fieldArrayList.remove(i);
+				i--;
 			}
 		}
 	}
@@ -129,6 +132,7 @@ public class Connector {
 			if(next instanceof Jail) {
 				goToJail.setJail(next);
 				fieldArrayList.remove(i);
+				i--;
 			}
 		}
 	}
@@ -136,8 +140,9 @@ public class Connector {
 	//JAVADOC
 	private void setDiceArray(Field[] map) {
 		int index;
-		Field[] diceArray = new Field[12];
+		Field[] diceArray;
 		for(int i = 0; i < map.length; i++) {
+			diceArray = new Field[11];
 			for(int j = 0; j < diceArray.length; j++) {
 				index = i + 2 + j;
 				if(index >= map.length) {
@@ -159,6 +164,7 @@ public class Connector {
 				for(Field f : map) {
 					if(f.getName().equals(name)) {
 						((GoTo) c).setField(f);
+						((GoTo) c).setGo(map[0]);
 						found = true;
 					}
 				}

Monopoly/src/core/Monopoly.java
@@ -68,8 +68,7 @@ public class Monopoly {
 		while(turnState != 2 && !gameOver) {
 			switch(menu.nextTurn(player, turnState)) {
 				case 0:
-					//					doubles = map.movePlayer(player);
-					player.getField().action(player);
+					doubles = player.move();
 					if(doubles) {
 						turnState = 3;
 					} else {
@@ -109,7 +108,7 @@ public class Monopoly {
 			}
 		}
 		if(doublesTime == 3) {
-			//			player.setField("Jail");
+			player.goToJail();
 			menu.inJail();
 		} else if(doubles) {
 			nextTurn(player, doublesTime + 1);

Monopoly/src/objects/Player.java
@@ -1,6 +1,8 @@
 package objects;
 
 import objects.map.Field;
+import objects.map.notPurchasable.Go;
+import objects.map.notPurchasable.Jail;
 import objects.map.purchasable.PurchasableCircularList;
 
 import java.util.HashMap;
@@ -14,9 +16,9 @@ public class Player {
 	private       boolean jailbaitCommunity;
 	private       int     money;
 	private       int[]   dices;
+	private       Field   field;
 	private HashMap<String, Field> ownedFieldMap   = new HashMap<String, Field>();
 	private Random                 randomGenerator = new Random(); //TODO have only one Random Generator for all Player
-	private Field field;
 
 	//JAVADOC
 	public Player(String name, int money) {
@@ -49,13 +51,13 @@ public class Player {
 			purchasable.getOwner().addMoney(purchasable.getPrice());
 		}
 		this.money -= purchasable.getPrice();
-		//		purchasable.setOwner(this);
+		purchasable.setOwner(this);
 		this.ownedFieldMap.put(purchasable.getName(), purchasable);
 	}
 
 	//JAVADOC
 	public void addMoney(int amount) {
-		this.setMoney(getMoney() + amount);
+		this.money += amount;
 	}
 
 	//JAVADOC
@@ -63,16 +65,13 @@ public class Player {
 		return this.money;
 	}
 
-	//JAVADOC
-	public void setMoney(int money) {
-		this.money = money;
+	public void setField(Field field) {
+		this.field = field;
 	}
 
 	//JAVADOC
-	public int[] throwDice() {
-		dices[0] = randomGenerator.nextInt(6) + 1;
-		dices[1] = randomGenerator.nextInt(6) + 1;
-		return dices;
+	public void setMoney(int money) {
+		this.money = money;
 	}
 
 	//JAVADOC
@@ -101,20 +100,59 @@ public class Player {
 	}
 
 	//JAVADOC
-	public void setField(Field field) {
-		setField(field, true);
+	public Field getField() {
+		return this.field;
 	}
 
 	//JAVADOC
-	public void setField(Field field, boolean doAction) {
-		this.field = field;
-		if(doAction) {
-			field.action(this);
+	public boolean move() {
+		throwDice();
+		boolean doubles = dices[0] == dices[1];
+		if(!inJail || !doubles) {
+			move(dices[0] + dices[1], true);
+		}
+		field.action(this);
+		return doubles;
+	}
+
+	//JAVADOC
+	private void move(int by, boolean overGo) {
+		if(field.getDiceNext(by) != null) {
+			field = field.getDiceNext(by);
+		} else {
+			for(int i = 0; i < by; i++) {
+				field = field.getNext();
+				if(overGo && field instanceof Go) {
+					field.action(this);
+				}
+			}
 		}
 	}
 
 	//JAVADOC
-	public Field getField() {
-		return this.field;
+	public int[] throwDice() {
+		dices[0] = randomGenerator.nextInt(6) + 1;
+		dices[1] = randomGenerator.nextInt(6) + 1;
+		return dices;
+	}
+
+	//JAVADOC
+	private boolean moveTo(Field newField) {
+		if(field.getFieldNumber() > newField.getFieldNumber()) {
+			field = newField;
+			return true;
+		} else {
+			field = newField;
+			return false;
+		}
+	}
+
+	//JAVADOC
+	//TODO improve - this shouldn't be done here
+	public void goToJail() {
+		while(!(field instanceof Jail)) {
+			field = field.getNext();
+		}
+		inJail = true;
 	}
 }

Monopoly/src/objects/card/GoTo.java
@@ -10,6 +10,7 @@ public class GoTo extends Card {
 	private final String  FIELDNAME;
 	private final boolean OVERGO;
 	private       Field   field;
+	private       Field   go;
 
 	//JAVADOC
 	public GoTo(String name, String text, String field, boolean overGo) {
@@ -26,5 +27,9 @@ public class GoTo extends Card {
 	//JAVADOC
 	public String getFieldName() {
 		return FIELDNAME;
+	}
+
+	public void setGo(Field field) {
+		go = field;
 	}
 }

Monopoly/src/objects/map/Field.java
@@ -55,4 +55,20 @@ public abstract class Field {
 		}
 		this.previous = previous;
 	}
+
+	public Field getNext() {
+		return next;
+	}
+
+	public int getFieldNumber() {
+		return fieldNumber;
+	}
+
+	public Field getDiceNext(int index) {
+		index -= 2;
+		if(fieldNumber < diceArray[index].getFieldNumber() && diceArray[index].getFieldNumber() != 0) {
+			return diceArray[index];
+		}
+		return null;
+	}
 }

Monopoly/src/objects/map/purchasable/PurchasableCircularList.java
@@ -44,15 +44,16 @@ public abstract class PurchasableCircularList extends Field {
 	public Player getOwner() {
 		return this.owner;
 	}
-	//	/**
-	//	 * @param owner The value determines the owner.
-	//	 */
-	//	public void setOwner(Player owner) {
-	//		this.owner = owner;
-	//		if(sameOwnerCheck(this)) {
-	//			stage = 1;
-	//		}
-	//	}
+
+	/**
+	 * @param owner The value determines the owner.
+	 */
+	public void setOwner(Player owner) {
+		this.owner = owner;
+		if(sameOwnerCheck(this)) {
+			stage = 1;
+		}
+	}
 
 	/**
 	 * @return The return value is the amount that has to be payed to become the owner.
@@ -92,32 +93,17 @@ public abstract class PurchasableCircularList extends Field {
 	public int getBill() {
 		return this.INCOME[this.stage];
 	}
-	//	/**
-	//	 * @param purchasable The value determines the next object in the circular list of objects that belong together.
-	//	 */
-	//	//TODO Move to constructor
-	//	public void add(PurchasableCircularList purchasable) {
-	//		purchasable.next = this.next;
-	//		this.next = purchasable;
-	//	}
-	//
-	//	/**
-	//	 * @return The return value is the reference to the next PurchasableCircularList object.
-	//	 */
-	//	private PurchasableCircularList getNext() {
-	//		return this.next;
-	//	}
-	//
-	//	//JAVADOC
-	//	private boolean sameOwnerCheck(PurchasableCircularList start) {
-	//		if(this.owner.equals(this.next.getOwner())) {
-	//			if(this.next.getNext().equals(start)) {
-	//				return true;
-	//			}
-	//			return this.next.sameOwnerCheck(start);
-	//		}
-	//		return false;
-	//	}
+
+	//JAVADOC
+	private boolean sameOwnerCheck(PurchasableCircularList start) {
+		if(this.owner.equals(this.nextGroupElement.getOwner())) {
+			if(this.nextGroupElement.getNextGroupElement().equals(start)) {
+				return true;
+			}
+			return this.nextGroupElement.sameOwnerCheck(start);
+		}
+		return false;
+	}
 
 	//JAVADOC
 	@Override