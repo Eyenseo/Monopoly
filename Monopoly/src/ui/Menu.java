Eyenseo/Monopoly

commit ed43b72425a7abb90c49c724095ff7f795696659

Implement the action of: Arrest, GoToStation, PayFineTakeCard, Payment, SpecialPayment, StreetWork Improve action of: GoBack, GoTo, Jailbait

Monopoly/src/ui/Menu.java
@@ -1,0 +1,18 @@
+package ui;
+
+import objects.Player;
+import objects.card.Card;
+
+import java.util.Vector;
+
+//JAVADOC
+public interface Menu {
+	//JAVADOC
+	public boolean showCardText(Card card);
+
+	//javaDoc
+	public void showInJail();
+
+	//JAVADOC
+	public int mainMenu(Player player, Vector<Player> playerVector, int turnState);
+}

Monopoly/src/ui/cui/ConsoleMenu.java
@@ -2,11 +2,14 @@ package ui.cui;
 
 import core.TradeManager;
 import objects.Player;
+import objects.card.Card;
+import objects.card.PayFineTakeCard;
 import objects.map.FieldCircularList;
 import objects.map.notPurchasable.Jail;
 import objects.map.purchasable.Facility;
 import objects.map.purchasable.PurchasableCircularList;
 import objects.map.purchasable.Street;
+import ui.Menu;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -18,7 +21,7 @@ import java.util.Vector;
  * @author Eyenseo
  * @version 1
  */
-public class ConsoleMenu {
+public class ConsoleMenu implements Menu {
 	private final Input in = new Input();
 
 	/**
@@ -201,7 +204,8 @@ public class ConsoleMenu {
 	 * Prints "Sie sind im Gefaengnis!"
 	 */
 	//TODO probably improve
-	public void inJail() {
+	@Override
+	public void showInJail() {
 		System.out.println("Sie sind im Gefaengnis!");
 	}
 
@@ -741,6 +745,7 @@ public class ConsoleMenu {
 	 * @param turnState    The value determines the current turn state.
 	 * @return The return value is updated turn state.
 	 */
+	@Override
 	public int mainMenu(Player player, Vector<Player> playerVector, int turnState) {
 		boolean end = false;
 		int choice = 0;
@@ -810,5 +815,16 @@ public class ConsoleMenu {
 			}
 		}
 		return turnState;
+	}
+
+	//JAVADOC
+	@Override
+	public boolean showCardText(Card card) {
+		System.out.println("\n" + card.getText() + "\n");
+		if(card instanceof PayFineTakeCard) {
+			println(((PayFineTakeCard) card).getOption());
+			return in.userBoolean('j', 'n', ((PayFineTakeCard) card).getOption());
+		}
+		return true;
 	}
 }

Monopoly/src/core/Connector.java
@@ -1,5 +1,6 @@
 package core;
 
+import objects.Player;
 import objects.card.*;
 import objects.exceptions.core.CardConnectionException;
 import objects.exceptions.core.NoInstanceException;
@@ -9,9 +10,11 @@ import objects.map.purchasable.Facility;
 import objects.map.purchasable.PurchasableCircularList;
 import objects.map.purchasable.Station;
 import objects.map.purchasable.Street;
+import ui.Menu;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Vector;
 
 /**
  * The Connector class connects the FieldCircularList objects and the CardStack objects.
@@ -21,19 +24,21 @@ import java.util.Collections;
  */
 public class Connector {
 	/**
-	 * @param map       The value determines the Array of FieldCircularList objects that shall be connected.
-	 * @param chance    The value determines the CardStack which has the Card objects from the chance file.
-	 * @param community The value determines the CardStack which has the Card objects from the community file.
+	 * @param map          The value determines the Array of FieldCircularList objects that shall be connected.
+	 * @param chance       The value determines the CardStack which has the Card objects from the chance file.
+	 * @param community    The value determines the CardStack which has the Card objects from the community file.
+	 * @param playerVector The value determines the Vector where all Player objects are stored.
+	 * @param menu         The value determines the Menu that will be used.
 	 * @throws NoInstanceException     The Exception is thrown if either no Jail, Parking, Go object exists.
 	 * @throws CardConnectionException The Exception is thrown if the Street for the GoTo Card is not found.
 	 */
-	public void connect(FieldCircularList[] map, CardStack chance, CardStack community)
-			throws NoInstanceException, CardConnectionException {
+	public void connect(FieldCircularList[] map, CardStack chance, CardStack community, Vector<Player> playerVector,
+	                    Menu menu) throws NoInstanceException, CardConnectionException {
 		makeCircularFieldList(map);
 		connectMatching(arrayToArrayList(map), chance, community);
 		setDiceArray(map);
-		connectCards(chance, map, community);
-		connectCards(community, map, community);
+		connectCards(chance, map, community, playerVector, menu);
+		connectCards(community, map, community, playerVector, menu);
 	}
 
 	/**
@@ -256,13 +261,16 @@ public class Connector {
 	}
 
 	/**
-	 * @param current   The value determines the current CardStack.
-	 * @param map       The value determines the FieldCircularList objects to check.
-	 * @param community The value determines the community CardStack.
+	 * @param current      The value determines the current CardStack.
+	 * @param map          The value determines the FieldCircularList objects to check.
+	 * @param community    The value determines the community CardStack.
+	 * @param playerVector The value determines the Vector where all Player objects are stored.
+	 * @param menu         The value determines what Menu will be used
 	 * @throws CardConnectionException The Exception is thrown if the Street for the GoTo Card is not found.
 	 */
-	private void connectCards(CardStack current, FieldCircularList[] map, CardStack community)
-			throws CardConnectionException {
+	//JAVADOC improve
+	private void connectCards(CardStack current, FieldCircularList[] map, CardStack community,
+	                          Vector<Player> playerVector, Menu menu) throws CardConnectionException {
 		boolean found;
 		for(Card c : current.getStack()) {
 			if(c instanceof GoTo) {
@@ -286,9 +294,29 @@ public class Connector {
 				}
 			} else if(c instanceof PayFineTakeCard) {
 				((PayFineTakeCard) c).setCommunity(community);
+				for(FieldCircularList f : map) {
+					if(f instanceof Parking) {
+						((PayFineTakeCard) c).setParking((Parking) f);
+					}
+				}
 			} else if(c instanceof Jailbait) {
 				((Jailbait) c).setCardStack(current);
+			} else if(c instanceof Payment) {
+				for(FieldCircularList f : map) {
+					if(f instanceof Parking) {
+						((Payment) c).setParking((Parking) f);
+					}
+				}
+			} else if(c instanceof StreetWork) {
+				for(FieldCircularList f : map) {
+					if(f instanceof Parking) {
+						((StreetWork) c).setParking((Parking) f);
+					}
+				}
+			} else if(c instanceof SpecialPayment) {
+				((SpecialPayment) c).setPlayerVector(playerVector);
 			}
+			c.setMenu(menu);
 		}
 	}
 }

Monopoly/src/core/Monopoly.java
@@ -7,6 +7,7 @@ import objects.exceptions.core.CardConnectionException;
 import objects.exceptions.core.NoInstanceException;
 import objects.exceptions.data.StorageReaderException;
 import objects.map.FieldCircularList;
+import ui.Menu;
 import ui.cui.ConsoleMenu;
 
 import java.util.Vector;
@@ -22,16 +23,16 @@ public class Monopoly {
 	private       boolean gameOver   = false;
 	private FieldCircularList go;
 	private FieldCircularList jail;
-	private ConsoleMenu       menu;
+	private Menu              menu;
 	private Vector<Player> playerVector = new Vector<Player>();
 
 	//TODO make a Menu Interface
-	Monopoly(ConsoleMenu menu) {
+	Monopoly(Menu menu) {
 		try {
 			MapArrayCreator mac = new MapArrayCreator();
 			CardStack event = new CardStack("events.txt", "Event Karte");
 			CardStack community = new CardStack("community.txt", "Gemeinschafts Karte");
-			new Connector().connect(mac.getMap(), event, community);
+			new Connector().connect(mac.getMap(), event, community, playerVector, menu);
 			this.go = mac.getGo();
 			this.jail = mac.getJail();
 			this.menu = menu;
@@ -87,7 +88,7 @@ public class Monopoly {
 			if(doublesTime == 3) {
 				player.setField(jail);
 				player.setInJail(true);
-				menu.inJail();
+				menu.showInJail();
 				turnState = 2;
 			} else if(turnState == 3) {
 				doublesTime++;
@@ -104,13 +105,13 @@ public class Monopoly {
 	}
 
 	public static void main(String[] args) {
-		ConsoleMenu menu = new ConsoleMenu();
+		Menu menu = new ConsoleMenu();
 		Monopoly m = new Monopoly(menu);
-		for(int i = menu.playerAmount(); i > 0; i--) {
-			m.addPlayer(new Player(menu.getName(), m.STARTMONEY));
-		}
-		//		m.addPlayer(new Player("Julia", m.STARTMONEY));
-		//		m.addPlayer(new Player("Markus", m.STARTMONEY));
+		//		for(int i = menu.playerAmount(); i > 0; i--) {
+		//			m.addPlayer(new Player(menu.getName(), m.STARTMONEY));
+		//		}
+		m.addPlayer(new Player("Julia", m.STARTMONEY));
+		m.addPlayer(new Player("Markus", m.STARTMONEY));
 		while(!m.isGameOver()) {
 			m.nextRound();
 		}

Monopoly/src/core/data/CardCreator.java
@@ -184,10 +184,11 @@ public class CardCreator extends StorageReader {
 		String text = null;
 		try {
 			text = nextString();
+			int dm = nextInt();
 			if(!isEndOfBlock()) {
 				throw new EndOfBlockException(path);
 			}
-			return new PayFineTakeCard(NAME, text);
+			return new PayFineTakeCard(NAME, text, dm);
 		} catch(Exception e) {
 			throw new PayFineTakeCardCreationException(NAME + ": " + text, e);
 		}

Monopoly/src/core/data/StorageReader.java
@@ -22,7 +22,7 @@ abstract class StorageReader {
 	/**
 	 * @param file The value determines which file will be loaded.
 	 * @throws StorageReaderException The Exception has a cause attribute that holds the previous Exception. It should be
-	 * read out with getMessageStack.
+	 *                                read out with getMessageStack.
 	 */
 	StorageReader(String file) throws StorageReaderException {
 		try {
@@ -36,8 +36,9 @@ abstract class StorageReader {
 
 	/**
 	 * @return The return value is the next valid line as int.
+	 *
 	 * @throws StorageReaderException The Exception has a cause attribute that holds the previous Exception. It should be
-	 * read out with getMessageStack.
+	 *                                read out with getMessageStack.
 	 */
 	int nextInt() throws StorageReaderException {
 		String line = nextString();
@@ -50,8 +51,9 @@ abstract class StorageReader {
 
 	/**
 	 * @return The return value is the next valid line as String.
+	 *
 	 * @throws StorageReaderException The Exception has a cause attribute that holds the previous Exception. It should be
-	 * read out with getMessageStack.
+	 *                                read out with getMessageStack.
 	 */
 	String nextString() throws StorageReaderException {
 		try {
@@ -65,6 +67,9 @@ abstract class StorageReader {
 			if(isEndOfBlock(line)) {
 				throw new EndOfBlockException(path);
 			}
+			String tempOne = "\\n";
+			String tempTwo = "\n";
+			line = line.replace(tempOne.subSequence(0, tempOne.length()), tempTwo.subSequence(0, tempTwo.length()));
 			return line;
 		} catch(IOException e) {
 			throw new StorageReaderException(e);
@@ -73,8 +78,8 @@ abstract class StorageReader {
 
 	/**
 	 * @return The return value is the next valid control word as String.
-	 * @throws StorageReaderException The Exception has a cause attribute that holds the previous Exception. It should be
-	 * read out with getMessageStack.
+	 *
+	 * @throws StorageReaderException The Exception has a cause attribute that holds the previous Exception. It should be read out with getMessageStack.
 	 */
 	String nextControlWord() throws StorageReaderException {
 		try {
@@ -101,8 +106,9 @@ abstract class StorageReader {
 
 	/**
 	 * @return The return value is true if the next line is the end of a data block.
+	 *
 	 * @throws StorageReaderException The Exception has a cause attribute that holds the previous Exception. It should be
-	 * read out with getMessageStack.
+	 *                                read out with getMessageStack.
 	 */
 	boolean isEndOfBlock() throws StorageReaderException {
 		try {

Monopoly/src/objects/card/Arrest.java
@@ -1,5 +1,6 @@
 package objects.card;
 
+import objects.Player;
 import objects.map.FieldCircularList;
 //JAVADOC
 
@@ -24,5 +25,13 @@ public class Arrest extends Card {
 	 */
 	public void setJail(FieldCircularList jail) {
 		this.jail = jail;
+	}
+
+	//JavaDoc
+	@Override
+	public void action(Player player) {
+		menu.showCardText(this);
+		player.setInJail(true);
+		player.setField(jail);
 	}
 }

Monopoly/src/objects/card/Card.java
@@ -1,7 +1,8 @@
 package objects.card;
+//JAVADOC
 
 import objects.Player;
-//JAVADOC
+import ui.Menu;
 
 /**
  * Card is the superclass of every card object.
@@ -9,7 +10,8 @@ import objects.Player;
 public abstract class Card {
 	private final String NAME;
 	private final String TEXT;
-	int index;
+	int  index;
+	Menu menu;
 
 	/**
 	 * @param name The value determines the name of the Card.
@@ -34,11 +36,8 @@ public abstract class Card {
 		return this.TEXT;
 	}
 
-	//	public abstract void action();
-	//TODO this has to be replaced in each individual Method and set to abstract
-	public void action(Player player) {
-		System.err.println("This is a placeholder lalala~lalaLA");
-	}
+	//JAVADOC
+	public abstract void action(Player player);
 
 	/**
 	 * @param index The value determines the index of the Card in the CardStack
@@ -46,5 +45,12 @@ public abstract class Card {
 	 */
 	public void setIndex(int index) {
 		this.index = index;
+	}
+
+	/**
+	 * @param menu The value determines what menu will be used.
+	 */
+	public void setMenu(Menu menu) {
+		this.menu = menu;
 	}
 }

commit 08d43d951e31bf6cfed05aafe842a7181742b0a8

Fix error introduced by commit ed43b72425a7abb90c49c724095ff7f795696659

Monopoly/src/ui/Menu.java
@@ -15,4 +15,10 @@ public interface Menu {
 
 	//JAVADOC
 	public int mainMenu(Player player, Vector<Player> playerVector, int turnState);
+
+	//JAVADOC
+	public int playerAmount();
+
+	//JAVADOC
+	public String getName();
 }

Monopoly/src/ui/cui/ConsoleMenu.java
@@ -29,6 +29,7 @@ public class ConsoleMenu implements Menu {
 	 *
 	 * @return The return value is the the name of a Player.
 	 */
+	@Override
 	public String getName() {
 		String name;
 		println("Wie heissen Sie?");
@@ -45,6 +46,7 @@ public class ConsoleMenu implements Menu {
 	 *
 	 * @return The return value is the number of player that want to play the game.
 	 */
+	@Override
 	public int playerAmount() {
 		println("Wie viele Spieler werden spielnen?");
 		return in.readInt();
@@ -78,7 +80,7 @@ public class ConsoleMenu implements Menu {
 	 * @param player   The value determines the payer to check against for the owner of the PurchasableCircularList objects.
 	 * @param property The value determines the PurchasableCircularList object of the group to be shown.
 	 */
-	public void groupDetails(Player player, PurchasableCircularList property) {
+	private void groupDetails(Player player, PurchasableCircularList property) {
 		System.out.println(propertiesDetails(player, property.getGroupMembers(), 0));
 	}
 
@@ -246,7 +248,7 @@ public class ConsoleMenu implements Menu {
 	 *
 	 * @param player The value determines the player who is currently at turn.
 	 */
-	public void playerPropertiesMenu(Player player) {
+	private void playerPropertiesMenu(Player player) {
 		int choice;
 		ArrayList<PurchasableCircularList> property = player.getProperties();
 		String menuOptions = "Sie haben " + player.getMoney() + " Geld.\n";
@@ -707,7 +709,7 @@ public class ConsoleMenu implements Menu {
 	 * @param buyer        The value determines the player who is the one to buy something.
 	 * @param playerVector The value determines the all playerVector
 	 */
-	public void tradeMenu(Player buyer, Vector<Player> playerVector) {
+	private void tradeMenu(Player buyer, Vector<Player> playerVector) {
 		int tradeState = 1;
 		Player seller = showPlayer(buyer, playerVector);
 		TradeManager tm = new TradeManager(buyer, seller);