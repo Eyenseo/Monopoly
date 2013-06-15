Eyenseo/Monopoly

commit 2dfb7e149de3f468a2a7ebf310f78d1e5325c083

HagglePanel: Better workaround for the decentered JSplitPane

Monopoly/src/ui/gui/components/HagglePanel.java
@@ -178,9 +178,10 @@ public class HagglePanel extends JPanel {
 	/**
 	 * The method is a work around without the split view doesn't splits in the center
 	 */
-	//TODO find a fix for decenter on first opening
-	public void centerSplit() {
+	@Override protected void paintComponent(Graphics g) {
+		//TODO find a better fix for decenter on first opening
 		hagglePane.setDividerLocation(0.5);
+		super.paintComponent(g);
 	}
 
 	/**

Monopoly/src/ui/gui/MainFrame.java
@@ -67,7 +67,6 @@ public class MainFrame extends JFrame {
 						break;
 					case HAGGLE:
 						mainPanel.add(hagglePanel, BorderLayout.CENTER);
-						hagglePanel.centerSplit();
 				}
 				validate();
 				repaint();

commit 7b52f62f10b81660fab2e3cb0132f85c00ca791e

HagglePanel: Add workaround for the bug introduced in 2dfb7e149de3f468a2a7ebf310f78d1e5325c083 The playerContent kept the old size and with that the LayoutManager lay out the components to long.

Monopoly/src/ui/gui/components/HagglePanel.java
@@ -181,6 +181,11 @@ public class HagglePanel extends JPanel {
 	@Override protected void paintComponent(Graphics g) {
 		//TODO find a better fix for decenter on first opening
 		hagglePane.setDividerLocation(0.5);
+
+		hagglePane.validate();
+		playerContent.setSize(new Dimension(hagglePane.getRightComponent().getWidth(), playerContent.getHeight()));
+		hagglePane.validate();
+
 		super.paintComponent(g);
 	}
 
@@ -300,7 +305,6 @@ public class HagglePanel extends JPanel {
 						}
 					}
 				}
-				hagglePane.setDividerLocation(0.5);
 			}
 		};
 