Eyenseo/Monopoly

commit b295c1ad07e0d9162cd879b7abf21071423b15bb

GUI: Rename client player and player to user - the user is the person who uses the GUI

Monopoly/src/ui/gui/CustomDialog.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
 //JAVADOC
 public class CustomDialog extends JFrame {
 	public CustomDialog(final Model model, final CardData cardData) {
-		super(cardData.getPlayerName() + ": " + cardData.getTyp());
+		super(cardData.getUserName() + ": " + cardData.getTyp());
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 

Monopoly/src/core/ClientOperator.java
@@ -67,8 +67,7 @@ public class ClientOperator {
 	 * @param haggleData the value determines the HaggleData to be checked
 	 */
 	public void updateHaggleData(HaggleData haggleData) {
-		if(haggleData.getPlayerId() == model.getClientPlayer().getId() ||
-		   haggleData.getSellerId() == model.getClientPlayer().getId()) {
+		if(haggleData.getUserId() == model.getUser().getId() || haggleData.getSellerId() == model.getUser().getId()) {
 			fireHaggleDataEvent(haggleData);
 		}
 	}
@@ -208,7 +207,7 @@ public class ClientOperator {
 	 * @param data the value determines the object that holds the PlayerDatas
 	 */
 	public void initializePlayer(InitializePlayer data) {
-		model.setPlayer(data.getPlayerData(), data.getPlayerId());
+		model.setPlayer(data.getPlayerData(), data.getUserId());
 		mainFrame = new MainFrame(model, this);
 		model.fireAllEvents();
 	}

Monopoly/src/core/Monopoly.java
@@ -99,28 +99,28 @@ public class Monopoly {
 	//TODO chop the method down
 	public void doAction(ActionData actionData) {
 		synchronized(turnThread) {
-			Player player = playerArrayList.get(actionData.getPlayerId());
+			Player user = playerArrayList.get(actionData.getUserId());
 
 			if(actionData instanceof TurnData) {
 				if(((TurnData) actionData).isTurnAction()) {
-					if(player.getThrewDice() < 3) {
-						player.move();
+					if(user.getThrewDice() < 3) {
+						user.move();
 					} else {
-						player.setPosition(jail);
-						player.setInJail(true);
-						player.setTurnEnd(true);
+						user.setPosition(jail);
+						user.setInJail(true);
+						user.setTurnEnd(true);
 						turnThread.notify();
 					}
 				} else {
-					player.setTurnEnd(true);
+					user.setTurnEnd(true);
 					turnThread.notify();
 				}
 			} else if(actionData instanceof BuyData) {
-				if(player.getPosition() instanceof PurchasableCircularList) { //TODO check if this is enough
+				if(user.getPosition() instanceof PurchasableCircularList) { //TODO check if this is enough
 					if(((BuyData) actionData).isUpgrade()) {
-						((Street) player.getPosition()).nextStage();
+						((Street) user.getPosition()).nextStage();
 					} else {
-						player.buyPurchasable((PurchasableCircularList) player.getPosition());
+						user.buyPurchasable((PurchasableCircularList) user.getPosition());
 					}
 				}
 			} else {
@@ -133,9 +133,9 @@ public class Monopoly {
 					switch(haggleData.getHaggleState()) {
 						case ESTABLISH:
 							seller = playerArrayList.get(haggleData.getSellerId());
-							if(player.getTrading() == -1 && seller.getTrading() == -1) {
-								player.setTrading(seller.getPlayerId());
-								seller.setTrading(player.getPlayerId());
+							if(user.getTrading() == -1 && seller.getTrading() == -1) {
+								user.setTrading(seller.getPlayerId());
+								seller.setTrading(user.getPlayerId());
 								haggleData.setHaggleState(HaggleData.HaggleState.ESTABLISHED);
 							} else {
 								haggleData.setHaggleState(HaggleData.HaggleState.DECLINE);
@@ -157,36 +157,36 @@ public class Monopoly {
 						case ACCEPT:
 							seller = playerArrayList.get(haggleData.getSellerId());
 							// trade money
-							int playerPay = haggleData.getSellerMoney() - haggleData.getPlayerMoney();
+							int playerPay = haggleData.getSellerMoney() - haggleData.getUserMoney();
 							if(playerPay != 0) {
 								if(playerPay < 0) {
 									playerPay *= -1;
 									seller.pay(playerPay);
-									player.addMoney(playerPay);
+									user.addMoney(playerPay);
 								} else {
 									seller.addMoney(playerPay);
-									player.pay(playerPay);
+									user.pay(playerPay);
 								}
 							}
 							// trade purchasables
 							for(int id : haggleData.getSellerFieldIds()) {
-								for(PurchasableCircularList purchasable : player.getProperties()) {
+								for(PurchasableCircularList purchasable : user.getProperties()) {
 									if(purchasable.getFieldNumber() == id) {
 										purchasable.setOwner(seller);
 										break;
 									}
 								}
 							}
-							for(int id : haggleData.getPlayerFieldIds()) {
+							for(int id : haggleData.getUserFieldIds()) {
 								for(PurchasableCircularList purchasable : seller.getProperties()) {
 									if(purchasable.getFieldNumber() == id) {
-										purchasable.setOwner(player);
+										purchasable.setOwner(user);
 										break;
 									}
 								}
 							}
 							serverOperator.sendHaggleData(haggleData);
-							player.setTrading(-1);
+							user.setTrading(-1);
 							seller.setTrading(-1);
 
 							break;
@@ -194,14 +194,14 @@ public class Monopoly {
 						case DECLINE:
 							seller = playerArrayList.get(haggleData.getSellerId());
 
-							player.setTrading(-1);
+							user.setTrading(-1);
 							seller.setTrading(-1);
 
 							serverOperator.sendHaggleData(haggleData);
 							break;
 					}
 				} else if(actionData instanceof MortgageData) {
-					ArrayList<PurchasableCircularList> purchasableCircularListArrayList = player.getProperties();
+					ArrayList<PurchasableCircularList> purchasableCircularListArrayList = user.getProperties();
 					for(PurchasableCircularList purchasable : purchasableCircularListArrayList) {
 						if(purchasable.getFieldNumber() == ((MortgageData) actionData).getFieldId()) {
 							purchasable.setInMortgage(!purchasable.isInMortgage());

Monopoly/src/objects/value/InitializePlayer.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class InitializePlayer {
 	private final ArrayList<PlayerData> playerData;
-	private final int                   playerId;
+	private final int                   userId;
 
 	/**
 	 * @param playerData the value determines the PlayerData of all players
-	 * @param playerId   the value determines the player id of the player of this client
+	 * @param userId     the value determines the player id of the player of this client
 	 */
-	public InitializePlayer(ArrayList<PlayerData> playerData, int playerId) {
+	public InitializePlayer(ArrayList<PlayerData> playerData, int userId) {
 		this.playerData = playerData;
-		this.playerId = playerId;
+		this.userId = userId;
 	}
 
 	/**
@@ -25,8 +25,8 @@ public class InitializePlayer {
 	/**
 	 * @return the return value is the player id of the player of this client
 	 */
-	public int getPlayerId() {
-		return playerId;
+	public int getUserId() {
+		return userId;
 	}
 }
 

Monopoly/src/objects/value/action/ActionData.java
@@ -4,19 +4,19 @@ package objects.value.action;
  * ActionData holds the player id of the player the data comes from
  */
 public abstract class ActionData {
-	private final int playerId;
+	private final int userId;
 
 	/**
-	 * @param playerId the value determines the player Id the data original from
+	 * @param userId the value determines the player Id the data original from
 	 */
-	ActionData(int playerId) {
-		this.playerId = playerId;
+	ActionData(int userId) {
+		this.userId = userId;
 	}
 
 	/**
 	 * @return the return value is the player Id the data original from
 	 */
-	public int getPlayerId() {
-		return playerId;
+	public int getUserId() {
+		return userId;
 	}
 }

Monopoly/src/objects/value/action/BuyData.java
@@ -7,11 +7,11 @@ public class BuyData extends ActionData {
 	private final boolean upgrade;
 
 	/**
-	 * @param playerId the value determines the player Id the data original from
-	 * @param upgrade  the value determines if a house/hotel will be bought or the field
+	 * @param userId  the value determines the player Id the data original from
+	 * @param upgrade the value determines if a house/hotel will be bought or the field
 	 */
-	public BuyData(int playerId, boolean upgrade) {
-		super(playerId);
+	public BuyData(int userId, boolean upgrade) {
+		super(userId);
 		this.upgrade = upgrade;
 	}
 

Monopoly/src/objects/value/action/GiveUpData.java
@@ -7,11 +7,11 @@ public class GiveUpData extends ActionData {
 	private final boolean giveUp;
 
 	/**
-	 * @param playerId the value determines the player Id the data original from
-	 * @param giveUp   the value determines if the player gives up or not
+	 * @param userId the value determines the player Id the data original from
+	 * @param giveUp the value determines if the player gives up or not
 	 */
-	public GiveUpData(int playerId, boolean giveUp) {
-		super(playerId);
+	public GiveUpData(int userId, boolean giveUp) {
+		super(userId);
 		this.giveUp = giveUp;
 	}
 

Monopoly/src/objects/value/action/HaggleData.java
@@ -7,20 +7,20 @@ import java.util.ArrayList;
  */
 public class HaggleData extends ActionData {
 	private final int                sellerId;       // id from the Player that something is Requested from
-	private final ArrayList<Integer> playerFieldIds; // ids from Purchasables that the player wants
+	private final ArrayList<Integer> userFieldIds;   // ids from Purchasables that the player wants
 	private final ArrayList<Integer> sellerFieldIds; // ids from Purchasables that the seller wants
 	private       HaggleState        haggleState;    // state of the trade
-	private       int                playerMoney;    // money that the Player (who sends the request) wants
+	private       int                userMoney;      // money that the Player (who sends the request) wants
 	private       int                sellerMoney;    // money that the seller (who sends th offer) wants
 
 	/**
-	 * @param playerId the value determines the player Id the data original from
+	 * @param userId   the value determines the player Id the data original from
 	 * @param sellerId the value determines the id of the Player that something is Requested from
 	 */
-	public HaggleData(int playerId, int sellerId) {
-		super(playerId);
+	public HaggleData(int userId, int sellerId) {
+		super(userId);
 		this.sellerId = sellerId;
-		playerFieldIds = new ArrayList<Integer>();
+		userFieldIds = new ArrayList<Integer>();
 		sellerFieldIds = new ArrayList<Integer>();
 		haggleState = HaggleState.ESTABLISH;
 	}
@@ -31,12 +31,12 @@ public class HaggleData extends ActionData {
 	 * @param haggleData the value determines a HaggleData to copy
 	 */
 	@Deprecated public HaggleData(HaggleData haggleData) {
-		super(haggleData.getPlayerId());
+		super(haggleData.getUserId());
 		sellerId = haggleData.getSellerId();
-		playerFieldIds = new ArrayList<Integer>(haggleData.getPlayerFieldIds());
+		userFieldIds = new ArrayList<Integer>(haggleData.getUserFieldIds());
 		sellerFieldIds = new ArrayList<Integer>(haggleData.getSellerFieldIds());
 		haggleState = haggleData.getHaggleState();
-		playerMoney = haggleData.getPlayerMoney();
+		userMoney = haggleData.getUserMoney();
 		sellerMoney = haggleData.getSellerMoney();
 	}
 
@@ -50,8 +50,8 @@ public class HaggleData extends ActionData {
 	/**
 	 * @return the return value is the amount of money that the Player (who sends the request) wants
 	 */
-	public int getPlayerMoney() {
-		return playerMoney;
+	public int getUserMoney() {
+		return userMoney;
 	}
 
 	/**
@@ -64,8 +64,8 @@ public class HaggleData extends ActionData {
 	/**
 	 * @return the return value is a ArrayList of ids from Purchasables that the player wants
 	 */
-	public ArrayList<Integer> getPlayerFieldIds() {
-		return playerFieldIds;
+	public ArrayList<Integer> getUserFieldIds() {
+		return userFieldIds;
 	}
 
 	/**
@@ -90,11 +90,10 @@ public class HaggleData extends ActionData {
 	}
 
 	/**
-	 * @param playerMoney the value determines the amount of money that the Player (who sends the request) wants
+	 * @param userMoney the value determines the amount of money that the Player (who sends the request) wants
 	 */
-	//TODO implement
-	public void setPlayerMoney(int playerMoney) {
-		this.playerMoney = playerMoney;
+	public void setUserMoney(int userMoney) {
+		this.userMoney = userMoney;
 	}
 
 	/**

commit 75ca27a7aa566613125fae6c5dd429c28584a577

CustomDialog: Fix error introduced in b295c1ad07e0d9162cd879b7abf21071423b15bb

Monopoly/src/ui/gui/CustomDialog.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
 //JAVADOC
 public class CustomDialog extends JFrame {
 	public CustomDialog(final Model model, final CardData cardData) {
-		super(cardData.getUserName() + ": " + cardData.getTyp());
+		super(cardData.getPlayerName() + ": " + cardData.getTyp());
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 