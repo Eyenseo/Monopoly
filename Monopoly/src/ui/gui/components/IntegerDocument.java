4753c2edb4d05db1121606841940e68e3cb9e828
Eyenseo <eyenseo@gmail.com>
2013-06-26 08:23:56 +0200

    IntegerDocument: Fix number can be larger than the integer max value

--- Monopoly/src/ui/gui/components/IntegerDocument.java
+++ Monopoly/src/ui/gui/components/IntegerDocument.java
@@ -6,9 +6,24 @@ import javax.swing.text.PlainDocument;
 
 //JAVADOC
 public class IntegerDocument extends PlainDocument {
+	private int maxLength;
+
 	//JAVADOC
+	public IntegerDocument() {
+		maxLength = 9;
+	}
+
+	//JAVADOC
+	public IntegerDocument(int maxLength) {
+		this.maxLength = maxLength;
+	}
+
+	//JAVADOC
+	//TODO don't cap at 9 digits but at the max intager value
 	@Override public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
 		if(str == null) {
+			return;
+		} else if(str.length() + offs > maxLength) {
 			return;
 		}
 
