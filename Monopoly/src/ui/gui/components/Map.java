45bcc6ad72c15ad20c7e6fd7d30e8675d4e1cbe2
Eyenseo <eyenseo@gmail.com>
2013-07-06 22:10:03 +0200

    Reup adding missing Data

--- Monopoly/src/ui/gui/components/Map.java
+++ Monopoly/src/ui/gui/components/Map.java
@@ -1,64 +1,51 @@
 package ui.gui.components;
 
-import objects.value.map.FacilityData;
-import objects.value.map.FieldData;
-import objects.value.map.StationData;
-import objects.value.map.StreetData;
+import objects.value.map.*;
 import ui.gui.Model;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 
 public class Map extends JPanel {
 	Map(LayoutManager layout, Model model) {
 		super(layout);
-		FieldCard card;
+		FieldCard card = null;
 		int[] colorData;
 		Color color;
 
-		for(FieldData data : model.getFieldDataArrayList()) {
-			if(data instanceof StreetData) {
-				StreetData sData = (StreetData) data;
-				colorData = sData.getCOLOR();
+		for(FieldData fieldData : model.getFieldDataArrayList()) {
+			if(fieldData instanceof StreetData) {
+				StreetData data = (StreetData) fieldData;
+				colorData = data.getCOLOR();
 				color = new Color(colorData[0], colorData[1], colorData[2]);
 
-				card = new FieldCard(sData.getName(), color, sData.getPRICE());
-			} else if(data instanceof FacilityData) {
-				FacilityData fData = (FacilityData) data;
+				card = new FieldCard(data.getName(), color, data.getPRICE());
+			} else if(fieldData instanceof FacilityData) {
+				FacilityData data = (FacilityData) fieldData;
 
-				card = new FieldCard(fData.getName(), "/storage/images/werk.png", fData.getPRICE());
-			} else if(data instanceof StationData) {
-				StationData stData = (StationData) data;
+				card = new FieldCard(data.getName(), "/storage/images/werk.png", data.getPRICE());
+			} else if(fieldData instanceof StationData) {
+				StationData data = (StationData) fieldData;
 
-				card = new FieldCard(stData.getName(), "/storage/images/bahnhof.png", stData.getPRICE());
-			} else {
-				card = new FieldCard("PLACEHOLDER", new Color(0, 0, 0), 555);
+				card = new FieldCard(data.getName(), "/storage/images/bahnhof.png", data.getPRICE());
+			} else if(fieldData instanceof GoData) {
+				card = new FieldCard(fieldData.getName(), "/storage/images/los.png");
+			} else if(fieldData instanceof TaxData) {
+				card = new FieldCard(fieldData.getName(), "/storage/images/steuern.png");
+			} else if(fieldData instanceof JailData) {
+				card = new FieldCard(fieldData.getName(), "/storage/images/gefaengnis.png");
+			} else if(fieldData instanceof ParkingData) {
+				card = new FieldCard(fieldData.getName(), "/storage/images/freiParken.png");
+			} else if(fieldData instanceof GoToJailData) {
+				card = new FieldCard(fieldData.getName(), "/storage/images/geheInsGefaengnis.png");
+			} else if(fieldData instanceof ChanceData) {
+				card = new FieldCard(fieldData.getName(), "/storage/images/ereignisfeld.png");
+			} else if(fieldData instanceof CommunityData) {
+				card = new FieldCard(fieldData.getName(), "/storage/images/gemeinschaftsfeld.png");
 			}
 
 			add(card);
 		}
 	}
-
-	@Override protected void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		AffineTransform backUpTransform = g2.getTransform();
-		AffineTransform transform = g2.getTransform();
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		Dimension preferredSize = getLayout().preferredLayoutSize(this);
-		double scaleX = (double) getWidth() / preferredSize.width;
-		double scaleY = (double) getHeight() / preferredSize.height;
-		double scale = 1;
-		if(scaleX > scaleY) {
-			scale = scaleY;
-		} else {
-			scale = scaleX;
-		}
-		transform.setToScale(scale, scale);
-		g2.setTransform(transform);
-		super.paintComponent(g);    //To change body of overridden methods use File | Settings | File Templates.
-		g2.setTransform(backUpTransform);
-	}
-
 	//HashMap<playerData, FieldCard>
 }
